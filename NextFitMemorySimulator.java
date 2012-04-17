dc19bbbdad97e7d4770ee1bca28d8c499992da5a
Dakota Szabo <szabod@rpi.edu>
2012-04-17 01:08:39 -0400

    NextFit done

--- NextFitMemorySimulator.java
+++ NextFitMemorySimulator.java
@@ -3,14 +3,23 @@ public class NextFitMemorySimulator extends MemorySimulatorBase {
 
 	public NextFitMemorySimulator(String fileName) {
 		super(fileName);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	protected int getNextSlot(int slotSize) {
-		// TODO Auto-generated method stub
-		//TODO Dakota
-		return 0;
+		int spaceAvailable = 0;
+		for (int i = main_memory.length-1; i >=0; i--) {
+			if (main_memory[i] == FREE_MEMORY) {
+				spaceAvailable++;
+			} else {
+				if (spaceAvailable < slotSize) {
+					return -1;
+				} else {
+					return i+1;
+				}
+			}
+		}
+		return -1;
 	}
 
 }
